326f70859962f3f858f476934df880730782b17a
tom <lim1017@hotmail.com>
2020-03-28 16:59:20 -0400

    asdf

--- src/index.js
+++ src/index.js
@@ -10,8 +10,6 @@ import "./assets/css/demo.css";
 import "./assets/css/pe-icon-7-stroke.css";
 import "./views/index.scss";
 
-// import '@fortawesome/fontawesome-free/css/all.min.css';
-// import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 
 
