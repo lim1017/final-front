f42a87e4b6312e965f50b914e04ca650d77a232f
tom <lim1017@hotmail.com>
2020-03-30 17:06:45 -0400

    button???

--- src/components/Goal/new.jsx
+++ src/components/Goal/new.jsx
@@ -1,6 +1,24 @@
 import React from "react";
+import MUButton from "@material-ui/core/Button";
+
 
 export default function New(props) {
+
+  const [button1, setButton1] = useState({
+    color: "linear-gradient(45deg, #ec407a 30%, #f48fb1 90%)",
+    x: 0
+  });
+
+  const style = {
+    background: "linear-gradient(45deg, #ec407a 30%, #f48fb1 90%)",
+    borderRadius: 15,
+    border: 0,
+    color: "white",
+    fontSize: 20
+  };
+
+
+
   return (
     <article className="goalNew goalCard">
       <div className="icons">
